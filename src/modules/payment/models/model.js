c81d871b4aa19a60939c25a15cb67e6f3076675d
Natsulelou <nutshapon_lelou@hotmail.com>
2020-03-11 17:23:55 +0700

    gen module

---
+++ src/modules/payment/models/model.js
@@ -0,0 +1,43 @@
+'use strict';
+// use model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var PaymentSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Please fill a Payment name',
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: {
+        type: Date
+    },
+    createby: {
+        _id: {
+            type: String
+        },
+        username: {
+            type: String
+        },
+        displayname: {
+            type: String
+        }
+    },
+    updateby: {
+        _id: {
+            type: String
+        },
+        username: {
+            type: String
+        },
+        displayname: {
+            type: String
+        }
+    }
+});
+
+mongoose.model("Payment", PaymentSchema);
---
+++ src/modules/payment/test/routes.js
@@ -0,0 +1,228 @@
+'use strict';
+var request = require('supertest'),
+    assert = require('assert'),
+    config = require('../../../config/config'),
+    _ = require('lodash'),
+    jwt = require('jsonwebtoken'),
+    mongoose = require('mongoose'),
+    app = require('../../../config/express'),
+    Payment = mongoose.model('Payment');
+
+var credentials,
+    token,
+    mockup;
+
+describe('Payment CRUD routes tests', function () {
+
+    before(function (done) {
+        mockup = {
+            name: 'name'
+        };
+        credentials = {
+            username: 'username',
+            password: 'password',
+            firstname: 'first name',
+            lastname: 'last name',
+            email: 'test@email.com',
+            roles: ['user']
+        };
+        token = jwt.sign(_.omit(credentials, 'password'), config.jwt.secret, {
+            expiresIn: 2 * 60 * 60 * 1000
+        });
+        done();
+    });
+
+    it('should be Payment get use token', (done)=>{
+        request(app)
+        .get('/api/payments')
+        .set('Authorization', 'Bearer ' + token)
+        .expect(200)
+        .end((err, res)=>{
+            if (err) {
+                return done(err);
+            }
+            var resp = res.body;
+            done();
+        });
+    });
+
+    it('should be Payment get by id', function (done) {
+
+        request(app)
+            .post('/api/payments')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .get('/api/payments/' + resp.data._id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) {
+                            return done(err);
+                        }
+                        var resp = res.body;
+                        assert.equal(resp.status, 200);
+                        assert.equal(resp.data.name, mockup.name);
+                        done();
+                    });
+            });
+
+    });
+
+    it('should be Payment post use token', (done)=>{
+        request(app)
+            .post('/api/payments')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                assert.equal(resp.data.name, mockup.name);
+                done();
+            });
+    });
+
+    it('should be payment put use token', function (done) {
+
+        request(app)
+            .post('/api/payments')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                var update = {
+                    name: 'name update'
+                }
+                request(app)
+                    .put('/api/payments/' + resp.data._id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .send(update)
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) {
+                            return done(err);
+                        }
+                        var resp = res.body;
+                        assert.equal(resp.data.name, update.name);
+                        done();
+                    });
+            });
+
+    });
+
+    it('should be payment delete use token', function (done) {
+
+        request(app)
+            .post('/api/payments')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .delete('/api/payments/' + resp.data._id)
+                    .set('Authorization', 'Bearer ' + token)
+                    .expect(200)
+                    .end(done);
+            });
+
+    });
+
+    it('should be payment get not use token', (done)=>{
+        request(app)
+        .get('/api/payments')
+        .expect(403)
+        .expect({
+            status: 403,
+            message: 'User is not authorized'
+        })
+        .end(done);
+    });
+
+    it('should be payment post not use token', function (done) {
+
+        request(app)
+            .post('/api/payments')
+            .send(mockup)
+            .expect(403)
+            .expect({
+                status: 403,
+                message: 'User is not authorized'
+            })
+            .end(done);
+
+    });
+
+    it('should be payment put not use token', function (done) {
+
+        request(app)
+            .post('/api/payments')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                var update = {
+                    name: 'name update'
+                }
+                request(app)
+                    .put('/api/payments/' + resp.data._id)
+                    .send(update)
+                    .expect(403)
+                    .expect({
+                        status: 403,
+                        message: 'User is not authorized'
+                    })
+                    .end(done);
+            });
+
+    });
+
+    it('should be payment delete not use token', function (done) {
+
+        request(app)
+            .post('/api/payments')
+            .set('Authorization', 'Bearer ' + token)
+            .send(mockup)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                var resp = res.body;
+                request(app)
+                    .delete('/api/payments/' + resp.data._id)
+                    .expect(403)
+                    .expect({
+                        status: 403,
+                        message: 'User is not authorized'
+                    })
+                    .end(done);
+            });
+
+    });
+
+    afterEach(function (done) {
+        Payment.deleteMany().exec(done);
+    });
+
+});
